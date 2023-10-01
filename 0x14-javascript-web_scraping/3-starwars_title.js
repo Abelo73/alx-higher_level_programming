8a575d85eddeac7251a3a06f67299b5c1a0a0f97
Abel Adisu <117712700+Abelo73@users.noreply.github.com>
2023-10-01 15:57:16 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,25 +1,7 @@
 #!/usr/bin/node
-const request = require('request');
-
-function getMovieTitle(movieId) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error(`Error: ${error}`);
-    } else if (response.statusCode !== 200) {
-      console.error(`Error: ${response.statusCode}`);
-    } else {
-      const movie = JSON.parse(body);
-      console.log(`Title: ${movie.title}`);
-    }
-  });
-}
-
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Error: Movie ID argument is missing');
-  process.exit(1);
-}
-
-getMovieTitle(movieId);
+const request = require("request");
+let url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
+});
